a1bc5c86e6ab32a0bb686daedbd8d78c218aec91
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-09 19:26:51 +0500

    create magic methods (str and other)

--- src/channel.py
+++ src/channel.py
@@ -6,16 +6,6 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    @classmethod
-    def get_service(cls):
-        """Возвращает специальный объект для работы с ютуб-API"""
-        return build('youtube', 'v3', developerKey=os.environ['YT_API_KEY'])
-
-    @property
-    def channel_id(self) -> str:
-        """Возвращает идентификатор канала"""
-        return self.__channel_id
-
     def __init__(self, channel_id: str):
         """При инициализации создаются атрибуты с основной информацией о канале"""
         self.__channel_id = channel_id
@@ -27,6 +17,41 @@ class Channel:
         self.subscribers_count = self.channel_summary_info['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel_summary_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_summary_info['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        """Выводит название и ссылку на канал по шаблону `<название_канала> (<ссылка_на_канал>)`"""
+        return f"'{self.title} ({self.url_channel})'"
+
+    def __add__(self, other):
+        return int(self.subscribers_count) + int(other.subscribers_count)
+
+    def __sub__(self, other):
+        return int(self.subscribers_count) - int(other.subscribers_count)
+
+    def __eq__(self, other):
+        return int(self.subscribers_count) == int(other.subscribers_count)
+
+    def __lt__(self, other):
+        return int(self.subscribers_count) < int(other.subscribers_count)
+
+    def __le__(self, other):
+        return int(self.subscribers_count) <= int(other.subscribers_count)
+
+    def __gt__(self, other):
+        return int(self.subscribers_count) >= int(other.subscribers_count)
+
+    def __ge__(self, other):
+        return int(self.subscribers_count) >= int(other.subscribers_count)
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает специальный объект для работы с ютуб-API"""
+        return build('youtube', 'v3', developerKey=os.environ['YT_API_KEY'])
+
+    @property
+    def channel_id(self) -> str:
+        """Возвращает идентификатор канала"""
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале в json-подобном удобном формате с отступами"""

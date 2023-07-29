556d422c2cb10b8d4d357efe773ae0b0805fd7c0
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-29 12:30:19 +0400

    implemented the "Playlist" class

--- src/video.py
+++ src/video.py
@@ -58,3 +58,29 @@ class PLVideo(Video):
     def playlist_id(self) -> str:
         """Возвращает идентификатор плейлиста"""
         return self.__playlist_id
+
+
+class Playlist:
+    """Создает экземпляры объектов с информацией о плейлисте Ютуб"""
+    def __init__(self, playlist_id: str):
+        self.__playlist_id: str = playlist_id
+        self.pl_summary_info = self.get_service().playlistItems().list(playlistId=self.__playlist_id,
+                                                                       part='contentDetails',
+                                                                       maxResults=50,
+                                                                       ).execute()
+        self.playlist_url = 'https://www.youtube.com/video/' + self.__playlist_id
+        self.pl_title = None
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает специальный объект для работы с АПИ Ютуб"""
+        return build(serviceName: 'youtube', version: 'v3', developerKey=os.environ['YT_API_KEY'])
+
+    @property
+    def playlist_id(self) -> str:
+        """Возвращает идентификатор плейлиста"""
+        return self.__playlist_id
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о видео-ролике в json-подобном удобном формате с отступами"""
+        print(json.dumps(self.pl_summary_info, indent=2, ensure_ascii=False))
